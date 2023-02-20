5ee338a7b7dc3b2eacacfc78f54ec78ecf7a512b
Grencione <76771267+Grencione@users.noreply.github.com>
2023-02-20 16:27:16 +0200

    Add files via upload

---
+++ 10.v_skolenu_darbi.py
@@ -0,0 +1,29 @@
+#Tatjana Grencione
+
+#1 Azarçvièa Arina
+
+#2 Bâra Antuanete
+
+#3 Boja Ernests
+
+#4 Donovs Emîls Niks
+
+#5 Feldmane Laura
+
+#6 Karpovièa Zlata
+
+#7 Kïimova Anna
+
+#8 Kokorevièa Diâna
+
+#9 Krastiòð Guntis
+
+#10 Loèmele Lîva
+
+#11 Milliòð Mâris
+
+#12 Plonis Gustavs
+
+#13 Strazdiòa Paula Marta
+
+#14 Vanags Renârs
