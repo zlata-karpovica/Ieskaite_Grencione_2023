b6cfdef2debca53e44ebd452fca1b012aac1476f
zlata-karpovica <124048837+zlata-karpovica@users.noreply.github.com>
2023-02-21 13:47:39 +0200

    Update 10.v_skolenu_darbi.py

--- 10.v_skolenu_darbi.py
+++ 10.v_skolenu_darbi.py
@@ -1,29 +1,29 @@
 #Tatjana Grencione
 
-#1 Azarçvièa Arina
+#1 AzarÃ§viÃ¨a Arina
 
-#2 Bâra Antuanete
+#2 BÃ¢ra Antuanete
 
 #3 Boja Ernests
 
-#4 Donovs Emîls Niks
+#4 Donovs EmÃ®ls Niks
 
 #5 Feldmane Laura
 
-#6 Karpovièa Zlata
+#6 KarpoviÃ¨a Zlata
+print(sin(radians(30)+fmod(13,2))) #RindÄ Nr.14 pievienoju matemÄtisku darbÄ«bu sin30+0.5
+#7 KÃ¯imova Anna
 
-#7 Kïimova Anna
+#8 KokoreviÃ¨a DiÃ¢na
 
-#8 Kokorevièa Diâna
+#9 KrastiÃ²Ã° Guntis
 
-#9 Krastiòğ Guntis
+#10 LoÃ¨mele LÃ®va
 
-#10 Loèmele Lîva
-
-#11 Milliòğ Mâris
+#11 MilliÃ²Ã° MÃ¢ris
 
 #12 Plonis Gustavs
 
-#13 Strazdiòa Paula Marta
+#13 StrazdiÃ²a Paula Marta
 
-#14 Vanags Renârs
+#14 Vanags RenÃ¢rs
